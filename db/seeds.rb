517ba613f07a189fe683e788f6f1b4ed712bada4
archie kenwright <kenwright.aw@googlemail.com>
2018-02-27 15:48:48 +0100

    aye

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating tasks..."
+Task.create!(title: "task1", details: "details", completed: false)
+Task.create!(title: "task2", details: "details", completed: false)
+Task.create!(title: "task3", details: "description", completed: false)
+
+puts "Completed."
